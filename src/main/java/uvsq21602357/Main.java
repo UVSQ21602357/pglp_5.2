83aa9a09fa4725861e162b3d65e829b422d4965d
Clément <clement.carre@ens.uvsq.fr>
2020-05-22 18:01:34 +0200

    JDBC dans main

--- src/main/java/uvsq21602357/Main.java
+++ src/main/java/uvsq21602357/Main.java
@@ -1,12 +1,17 @@
 package uvsq21602357;
 
+import java.sql.SQLException;
+
 public enum Main {
 
 APPLICATION;
 
 public void run(final String[] args) { }
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws ClassNotFoundException, SQLException {
+    	JDBC j = new JDBC();
+    	j.CreateTable();
+    	
      APPLICATION.run(args);
     }
 
